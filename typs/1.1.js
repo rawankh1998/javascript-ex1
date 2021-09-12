a3028905279b4d937404a19fae7d5d28a85decec
rawankh1998 <rawankh1998@gmail.com>
2021-09-12 10:52:04 +0300

    first commit

---
+++ typs/1.1.js
@@ -0,0 +1,17 @@
+let str="Hello worls!"
+let num="10";
+let str2="$";
+let gameover= false;
+let y="null";
+let x;
+let sym3 = Symbol('key')
+
+console.log(str);
+console.log(num);
+console.log(str2);
+console.log(y);
+console.log(x);
+console.log(gameover);
+console.log(sym3)
+
+

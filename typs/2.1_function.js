6721f059eb3b7270532e29f025a71e7180c89f78
rawankh1998 <rawankh1998@gmail.com>
2021-09-12 13:37:16 +0300

    first commit

---
+++ typs/2.1_function.js
@@ -0,0 +1,8 @@
+function describeCountry(country,population,capitalCity){
+   
+    return `${country} has ${population} million people and its capital city is ${capitalCity} `
+    
+}
+console.log(describeCountry('finland',6,'Helsinki'));
+console.log(describeCountry('Isreal',7.8,'Juroslem'));
+console.log(describeCountry('Uk',9,'London'));
